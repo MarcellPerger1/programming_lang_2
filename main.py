cb954ad5e4e7af837257eb18020ed0ac8c1d31a6
Marcell Perger <marcell.perger.1@gmail.com>
2023-07-22 21:30:43 +0100

    perf: Add profiler to `main.py`

--- main.py
+++ main.py
@@ -14,15 +14,25 @@ def read_file(path: str):
         return f.read()
 
 
-def run(src: str):
+PROFILER = True
+
+
+def run(src: str, idx: int = -1):
+    import cProfile
     tn0 = time.perf_counter()
     tn = Tokenizer(src).tokenize()
     tn1 = time.perf_counter()
     print('Tokens:')
     print_tokens(tn.src, tn.tokens)
     tp0 = time.perf_counter()
-    node = TreeGen(tn).parse()
-    tp1 = time.perf_counter()
+    if PROFILER:
+        with cProfile.Profile() as p:
+            node = TreeGen(tn).parse()
+        tp1 = time.perf_counter()
+        p.dump_stats(f'perf_dump_{idx}.prof')
+    else:
+        node = TreeGen(tn).parse()
+        tp1 = time.perf_counter()
     print('CST:')
     tprint(node)
     print(f'Tokens done in {(tn1 - tn0) * 1000:.2f}ms')
@@ -30,8 +40,8 @@ def run(src: str):
 
 
 def main():
-    run(read_file('main_example_0.txt'))
-    run(read_file('main_example_1.txt'))
+    run(read_file('main_example_0.txt'), 0)
+    run(read_file('main_example_1.txt'), 1)
 
 
 if __name__ == '__main__':
