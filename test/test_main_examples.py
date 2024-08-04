b3eabe220f43a894e74f3f680b7050f42808efae
Marcell Perger <marcell.perger.1@gmail.com>
2024-08-04 19:24:59 +0100

    test: Test main.py to check no exceptions thrown.
    TODO: snapshot test them

---
+++ test/test_main_examples.py
@@ -0,0 +1,12 @@
+import unittest
+
+from main import main
+
+
+class TestMain(unittest.TestCase):
+    def test(self):
+        main()  # TODO: add some sort of snapshot for main?
+
+
+if __name__ == '__main__':
+    unittest.main()
