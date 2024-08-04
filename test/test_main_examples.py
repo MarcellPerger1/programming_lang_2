6c02a010444c2bf11c5d35c235c5c0600d4d5b81
Marcell Perger <marcell.perger.1@gmail.com>
2024-08-04 19:30:51 +0100

    fix: Fix working dir issues

--- test/test_main_examples.py
+++ test/test_main_examples.py
@@ -1,9 +1,17 @@
+import os
 import unittest
+from pathlib import Path
 
 from main import main
 
 
 class TestMain(unittest.TestCase):
+    def setUp(self):
+        if not Path('./main_example_0.st').exists():
+            self.old_dir = os.getcwd()
+            os.chdir(Path(__file__).parent.parent)
+            self.addCleanup(lambda : os.chdir(self.old_dir))
+
     def test(self):
         main()  # TODO: add some sort of snapshot for main?
 
