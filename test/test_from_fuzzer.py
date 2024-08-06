d430dbb67c70c68bf96a67ef70e4b99c5b85fdb5
Marcell Perger <marcell.perger.1@gmail.com>
2024-08-06 23:50:42 +0100

    test: Add test for timeout in infinite loop

--- test/test_from_fuzzer.py
+++ test/test_from_fuzzer.py
@@ -4,8 +4,10 @@ from parser.lexer.tokenizer import Tokenizer
 from parser.cst.treegen import TreeGen
 from parser.error import BaseParseError
 
+from test.utils import timeout_decor
 
-class FuzzerTestCase(unittest.TestCase):
+
+class FuzzerCrashTestCase(unittest.TestCase):
     """Tests for crashes caught by pythonfuzz"""
 
     def assertNotInternalError(self, src: str):
@@ -17,6 +19,32 @@ class FuzzerTestCase(unittest.TestCase):
     def test_3610f59833246958fff7d5cbc5b23f8c99496c3c8fda3f5606f5b198713cbb95(self):
         self.assertNotInternalError('. ')
         self.assertNotInternalError('.')
+
+
+class FuzzerTimeoutTestCase(unittest.TestCase):
+    def assertNotInternalError(self, src: str):
+        try:
+            TreeGen(Tokenizer(src)).parse()
+        except BaseParseError:
+            pass
+
+    @staticmethod
+    def raiseInternalErrorOrNone(src: str):
+        try:
+            TreeGen(Tokenizer(src)).parse()
+        except BaseParseError:
+            return None
+        except Exception as ex:
+            raise ex
+        return None
+
+    @staticmethod
+    @timeout_decor(5, debug=0)
+    def inner_a32460d584fd8a20d1e62007db570eaf41342f248e42c21a33780fd976e45290():
+        return FuzzerTimeoutTestCase.raiseInternalErrorOrNone('s/*y')
+
+    def test_a32460d584fd8a20d1e62007db570eaf41342f248e42c21a33780fd976e45290(self):
+        self.inner_a32460d584fd8a20d1e62007db570eaf41342f248e42c21a33780fd976e45290()
 
 
 if __name__ == '__main__':
