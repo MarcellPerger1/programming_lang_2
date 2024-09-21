da247de74c44ca80ebf64e7cd28e41730ac2123a
Marcell Perger <marcell.perger.1@gmail.com>
2024-09-21 19:14:10 +0100

    fix: Fix DivisionByZero in pythonfuzz in a way that doesn't modify its files

--- fuzz.py
+++ fuzz.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import time
 
 from parser.lexer.tokenizer import Tokenizer
 from parser.cst.treegen import TreeGen
@@ -15,13 +16,25 @@ def new_ssm(m: str):
 multiprocessing.set_start_method = new_ssm  # Monkey-patch it to make pythonfuzz work
 
 
-# TODO: make this work on CI?
-# To get ths to work (Win11): Change
-# execs_per_second = int(self._executions_in_sample / (endTime - self._last_sample_time))
-# to
-# execs_per_second = int(self._executions_in_sample / (endTime - self._last_sample_time or 0.000001))  # noqa (line length)
-
 from pythonfuzz.fuzzer import Fuzzer  # noqa (import too low down)
+import pythonfuzz.fuzzer as fuzzer_ns  # noqa (import too low down)
+
+
+class UsePerfCounterInsteadOfTime:
+    """Hack to avoid overwriting everyone's time module so we only
+    overwrite `pythonfuzz`'s time module and don't modify the module itself.
+    This hack is necessary because time.time() is rather inaccurate so
+    it is possible that between 2 iterations, the difference in time.time() is 0
+    which results in DivisionByZeroError (when calculating iterations/sec).
+    Therefore, we replace with the more accurate time.perf_counter(),
+    just for `pythonfuzz`"""
+    def __getattr__(self, item):
+        if item == 'time':  # time.time
+            item = 'perf_counter'
+        return getattr(time, item)
+
+
+fuzzer_ns.time = UsePerfCounterInsteadOfTime()
 
 
 def fuzz(buf):
@@ -36,8 +49,14 @@ def fuzz(buf):
 
 
 # pickling errors... (old func would be inaccessible using decorator)
-fuzzer = Fuzzer(fuzz, dirs=['./pythonfuzz_corpus'], timeout=30)
-
-
 if __name__ == '__main__':
+    import argparse
+    ap = argparse.ArgumentParser("fuzz.py", description="Runs a fuzzer for n iterations")
+    ap.add_argument('-n', '--iterations', default=1_000_000,
+                    type=int, help="Number of iterations to run pythonfuzz for")
+    ap.add_argument('-i', '--infinite',
+                    action='store_const', const=-1, dest='iterations')
+    args = ap.parse_args()
+
+    fuzzer = Fuzzer(fuzz, dirs=['./pythonfuzz_corpus'], timeout=30, runs=args.iterations)
     fuzzer.start()
