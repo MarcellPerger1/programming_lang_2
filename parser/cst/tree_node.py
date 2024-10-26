19ae44b872ab2d72031b54c001192d3125aff963
Marcell Perger <marcell.perger.1@gmail.com>
2024-10-26 22:16:07 +0100

    refactor: Add infrastructure for CST node hierarchy

--- parser/cst/tree_node.py
+++ parser/cst/tree_node.py
@@ -9,6 +9,9 @@ if TYPE_CHECKING:
     from ..tokens import Token
 
 
+# NOTE: these classes only follow the Liskov substitution principle on
+# instances, not on __init__ and similar classmethods.
+
 @dataclass
 class Leaf:
     name: str
@@ -18,6 +21,22 @@ class Leaf:
     @classmethod
     def of(cls, token: Token, name: str | None = '', parent: Node | None = None):
         return cls(name or token.name, token.region, parent)
+
+
+@dataclass
+class NamedLeafCls(Leaf):
+    """A Leaf with a class-defined name"""
+    name: str = field(init=False, repr=False)
+
+    def __post_init__(self):
+        if type(self) is NamedLeafCls:
+            raise TypeError("NamedLeafCls may not be instantiated directly;"
+                            " use a subclass or use Leaf")
+
+    # noinspection PyMethodOverriding
+    @classmethod
+    def of(cls, token: Token, parent: Node | None = None):
+        return cls(token.region, parent)
 
 
 AnyNode = Leaf
@@ -53,3 +72,47 @@ class Node(Leaf):
             n.parent = n
         if update_end:
             self.region.end = end
+
+
+@dataclass
+class NamedNodeCls(Node):
+    """A Node with a class-defined name"""
+    name: str = field(init=False, repr=False)
+
+    def __post_init__(self):
+        if type(self) is NamedNodeCls:
+            raise TypeError("NamedNodeCls may not be instantiated directly;"
+                            " use a subclass or use Node")
+
+    # noinspection PyMethodOverriding
+    @classmethod
+    def of(cls, token: Token, children: list[AnyNode] | None = None,
+           parent: Node | None = None):
+        return cls(token.region, parent, children or [])
+
+    # noinspection PyMethodOverriding
+    @classmethod  # Better args order
+    def new(cls, region: StrRegion, children: list[AnyNode], parent: Node = None):
+        return cls(region, parent, children)
+
+
+class NamedSizedNodeCls(NamedNodeCls):
+    """A Node with a class-defined name and size."""
+    size: int
+
+    def __post_init__(self):
+        if type(self) is NamedNodeCls:
+            raise TypeError("NamedSizedNodeCls may not be instantiated directly;"
+                            " use a subclass or use Node")
+        children = self.children
+        if len(children) != self.size:
+            raise ValueError(f"{type(self).__name__} expected {self.size} "
+                             f"children, got {len(children)}")
+        self.children = []
+        self._add(*children)
+
+    def _add(self, *nodes: AnyNode, update_end=False):
+        super().add(*nodes, update_end=update_end)
+
+    def add(self, *nodes: AnyNode, update_end=False):
+        raise TypeError(f"Cannot add nodes to fixed size {type(self).__name__}")
