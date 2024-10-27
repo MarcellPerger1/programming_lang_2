81a518d08d7f4a96bfbf9b23d2e4d08ab52fd353
Marcell Perger <marcell.perger.1@gmail.com>
2024-10-27 16:11:16 +0000

    refactor(tree-print): Add verbose mode and refactor.

--- parser/cst/tree_print.py
+++ parser/cst/tree_print.py
@@ -5,6 +5,7 @@ from io import StringIO
 from typing import IO, Sequence
 
 from .tree_node import Node, Leaf
+from ..str_region import StrRegion
 
 __all__ = [
     'TreePrinter', 'tree_print', 'tree_format', 'tprint', 'tformat'
@@ -12,8 +13,9 @@ __all__ = [
 
 
 class TreePrinter:
-    def __init__(self, stream: IO[str] = None, indent=2):
+    def __init__(self, stream: IO[str] = None, indent=2, verbose=False):
         self.indent = indent
+        self.verbose = verbose
         if stream is None:
             stream = sys.stdout
         self.stream = stream
@@ -23,10 +25,9 @@ class TreePrinter:
         self.stream.write('\n')
 
     def format(self, obj: object):
-        out = StringIO()
         orig_stream = self.stream
+        self.stream = out = StringIO()
         try:
-            self.stream = out
             self.print(obj)
         finally:
             self.stream = orig_stream
@@ -44,44 +45,47 @@ class TreePrinter:
         else:
             self._write_fallback(obj, level)
 
-    def _write_seq(self, obj: Sequence, level: int, char_start: str,
-                   char_end: str, require_trailing=False):
-        indent = ' ' * level * self.indent
-        self.stream.write(f'{indent}{char_start}')
+    def _write_seq(self, obj: Sequence, level: int, start: str,
+                   end: str, require_trailing=False):
+        self._indented_write(level, start)
         if len(obj) == 0:
-            self.stream.write(f'{char_end}')
+            self.stream.write(end)
             return
-        else:
-            self.stream.write('\n')
+        self.stream.write('\n')  # Start items on new line
         for i, c in enumerate(obj):
             self._write(c, level + 1)
             if i != len(obj) - 1:
                 self.stream.write(',\n')
             elif require_trailing:
                 self.stream.write(',')
-        self.stream.write(f'\n{indent}{char_end}')
+        self.stream.write('\n')  # Put ']' on new line
+        self._indented_write(level, end)
 
     def _write_node(self, obj: Node, level: int):
-        indent = ' ' * level * self.indent
-        self.stream.write(f'{indent}Node({obj.name}, [')
-        if len(obj.children) == 0:
-            self.stream.write('])')
-            return
+        if not self.verbose:
+            start = f'Node({obj.name}, ['
         else:
-            self.stream.write('\n')
-        for i, c in enumerate(obj.children):
-            self._write(c, level + 1)
-            if i != len(obj.children) - 1:
-                self.stream.write(',\n')
-        self.stream.write(f'\n{indent}])')
+            start = f'Node({obj.name}, {self._fmt_region(obj.region)}, ['
+        self._write_seq(obj.children, level, start, end='])')
 
     def _write_leaf(self, obj: Leaf, level: int):
-        indent = ' ' * level * self.indent
-        self.stream.write(f'{indent}Leaf({obj.name})')
+        if not self.verbose:
+            self._indented_write(level, f'Leaf({obj.name})')
+        else:
+            self._indented_write(
+                level, f'Leaf({obj.name}, {self._fmt_region(obj.region)})')
 
     def _write_fallback(self, obj: object, level: int):
-        indent = ' ' * level * self.indent
-        self.stream.write(f'{indent}{obj!r}')
+        self._indented_write(level, repr(obj))
+
+    def _indented_write(self, level: int, s: str):
+        if level:
+            self.stream.write(' ' * level * self.indent)
+        self.stream.write(s)
+
+    @classmethod
+    def _fmt_region(cls, r: StrRegion):
+        return f'StrRegion({r.start}, {r.end})'
 
 
 def tree_print(obj: object, stream: IO[str] = None, indent: int = 2):
