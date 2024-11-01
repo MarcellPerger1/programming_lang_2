60aeb0f8dad9d43e74e1e333da360c4c9950f5c4
Marcell Perger <marcell.perger.1@gmail.com>
2024-11-01 19:09:06 +0000

    fix(tokenizer):Fix circular import issue I accidentally caused

--- parser/common/__init__.py
+++ parser/common/__init__.py
@@ -1,4 +1,7 @@
 from .common import *
 from .error import *  # <^ might add some stuff so `import *`
 from .str_region import StrRegion  # <-- won't add stuff to str_region so not `import *`
-from .tree_print import *
+# IMPORTant: don't include tree_print here as that causes circular import issue:
+#  - lexer.tokens imports ..common (for StrRegion)
+#  - tree_print also loaded from common/__init__.py
+#  - tree_print needs `Node`... and `Node` needs lexer.tokens
