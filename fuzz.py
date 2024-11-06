3356879e14687a3fe137a0132644e03560c3b200
Marcell Perger <marcell.perger.1@gmail.com>
2024-11-06 17:04:37 +0000

    test(ast): Use fuzzing for AST

--- fuzz.py
+++ fuzz.py
@@ -1,5 +1,6 @@
 import time
 
+from parser.astgen.astgen import AstGen
 from parser.lexer.tokenizer import Tokenizer
 from parser.cst.treegen import TreeGen
 from parser.common.error import BaseParseError
@@ -29,7 +30,7 @@ def fuzz(buf):
     try:
         string = buf.decode("ascii")
         try:
-            TreeGen(Tokenizer(string)).parse()
+            AstGen(TreeGen(Tokenizer(string))).walk()
         except BaseParseError:
             pass
     except UnicodeDecodeError:
