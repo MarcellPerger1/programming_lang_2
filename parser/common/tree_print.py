f302955e1a5336450a26d7c93d4a1741af2db981
Marcell Perger <marcell.perger.1@gmail.com>
2024-11-07 18:53:43 +0000

    feat(ast): Add AST node handling to tree_print.py

--- parser/common/tree_print.py
+++ parser/common/tree_print.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+import dataclasses as dcls
+import enum
 import sys
 from io import StringIO
 from typing import IO, Sequence
 
+from ..astgen.ast_node import AstNode
 from ..cst.base_node import Leaf, AnyNode, Node
 from .str_region import StrRegion
 
@@ -36,14 +39,20 @@ class TreePrinter:
         return out.getvalue()
 
     def _write(self, obj: object, level: int):
-        if isinstance(obj, Node):
-            self._write_node(obj, level)
+        if isinstance(obj, StrRegion):
+            self._indented_write(level, self._fmt_region(obj))
+        elif isinstance(obj, Node):
+            self._write_cst_node(obj, level)
         elif isinstance(obj, Leaf):
-            self._write_leaf(obj, level)
+            self._write_cst_leaf(obj, level)
+        elif isinstance(obj, AstNode):
+            self._write_ast_node(obj, level)
         elif isinstance(obj, list):
             self._write_seq(obj, level, '[', ']')
         elif isinstance(obj, tuple):
             self._write_seq(obj, level, '(', ')', True)
+        elif isinstance(obj, enum.Enum):
+            self._indented_write(level, f'{type(obj).__name__}.{obj.name}')
         else:
             self._write_fallback(obj, level)
 
@@ -58,25 +67,31 @@ class TreePrinter:
             self._write(c, level + 1)
             if i != len(obj) - 1:
                 self.stream.write(',\n')
-            elif require_trailing:
+            elif require_trailing and len(obj) == 1:
                 self.stream.write(',')
         self.stream.write('\n')  # Put ']' on new line
         self._indented_write(level, end)
 
-    def _write_node(self, obj: Node, level: int):
+    def _write_cst_node(self, obj: Node, level: int):
         start = self._fmt_node_header(obj, has_more_args=True) + '['
         self._write_seq(obj.children, level, start, end='])')
 
-    def _write_leaf(self, obj: Leaf, level: int):
+    def _write_cst_leaf(self, obj: Leaf, level: int):
         self._indented_write(level, self._fmt_node_header(obj, has_more_args=False) + ')')
 
-    def _write_fallback(self, obj: object, level: int):
-        self._indented_write(level, repr(obj))
-
-    def _indented_write(self, level: int, s: str):
-        if level:
-            self.stream.write(' ' * level * self.indent)
-        self.stream.write(s)
+    def _write_ast_node(self, obj: AstNode, level: int):
+        # TODO Not the ideal formatting (but I don't know what is.
+        values = [getattr(obj, f.name) for f in dcls.fields(obj)]
+        if any(isinstance(v, (AnyNode, AstNode, list, tuple)) for v in values):
+            assert len(values) > 1  # Otherwise wouldn't be complex
+            start = f'{type(obj).__name__}({self._fmt_region(obj.region)},'
+            return self._write_seq(values[1:], level, start, end=')')
+        self._indented_write(level, f'{type(obj).__name__}(')
+        for i, v in enumerate(values):
+            self._write(v, 0)
+            if i != len(values) - 1:
+                self.stream.write(', ')
+        self.stream.write(')')
 
     def _fmt_node_header(self, obj: AnyNode, has_more_args=True):
         args: list[str] = []
@@ -87,6 +102,14 @@ class TreePrinter:
         if has_more_args:
             args.append('')  # Placeholder for next arg so ', ' gets added
         return f'{type(obj).__name__}({", ".join(args)}'
+
+    def _write_fallback(self, obj: object, level: int):
+        self._indented_write(level, repr(obj))
+
+    def _indented_write(self, level: int, s: str):
+        if level:
+            self.stream.write(' ' * level * self.indent)
+        self.stream.write(s)
 
     @classmethod
     def _fmt_region(cls, r: StrRegion):
