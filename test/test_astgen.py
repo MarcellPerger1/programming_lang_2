f08499a286690274ba379f798220f07d10ca996e
Marcell Perger <marcell.perger.1@gmail.com>
2024-11-10 22:59:48 +0000

    test: Add test for NopNode-handling

--- test/test_astgen.py
+++ test/test_astgen.py
@@ -4,3 +4,7 @@ from test.common import CommonTestCase
 class TestAstGen(CommonTestCase):
     def test_op_node(self):
         self.assertAstMatchesSnapshot('s=s+9;')
+
+    def test_nop_node(self):
+        self.assertAstMatchesSnapshot('if(2){;}else if (1==2){;a();;} else {};\n'
+                                      ';repeat(a){;};def fn(){;};')
