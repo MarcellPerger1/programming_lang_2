d6b81e308ace2077ff14e38021f31163f9c4e631
Marcell Perger <marcell.perger.1@gmail.com>
2024-11-10 22:13:06 +0000

    fix: Fix injection for functions/classes page

--- test/coverage/inject_me.js
+++ test/coverage/inject_me.js
@@ -27,22 +27,12 @@ function linkifyRow(tr) {
   [...tr.children].forEach((el, i) => {  // Copy to avoid problems with iterating while modifying
     console.log(el, i);
     if(i==0) return;
+    if(el.querySelector('td > a')) return;  // Already has an <a>
     let aNew = document.createElement('a');
     aNew.href = href;
     aNew.append(...el.childNodes);  // Transfer all children to inner <a>
     el.append(aNew);
   })
-//  let name = a.textContent;
-//  a.textContent = '';  // Delete all children
-//  a.remove();  // Disconnect from main DOM
-//
-//  let ch;
-//  while(ch=tr.firstChild) {
-//    a.append(ch);  // Moves it from <tr> to <a>
-//  }
-//  nameField.textContent = name;
-//  tr.insertBefore(a, tr.firstChild);  // Put <a> back into DOM, in <tr>
-
 }
 function linkifyAll() {
   document.querySelectorAll('tr.region').forEach(linkifyRow);
@@ -53,20 +43,3 @@ addEventListener('load', () => {
     linkifyAll();
   }
 });
-// Pseudocode:
-
-// foreach tr
-// inner = new(<a> </a>)
-// for child in tr: inner.append(child)  # Move it (cannot have 2 parents)
-// inner[0].append(inner[0][0].text); inner[0].delitem(0);
-// tr.append(inner)
-
-//#index a {
-//  text-decoration: none;
-//  color: inherit;
-//}
-
-//#index a:hover {
-//  text-decoration: underline;
-//  color: inherit;
-//}
