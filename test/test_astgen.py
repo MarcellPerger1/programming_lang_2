3d2073a026410601550768c14c660c3c523f556a
Marcell Perger <marcell.perger.1@gmail.com>
2024-11-12 17:34:45 +0000

    test: Add more tests for AstGen

--- test/test_astgen.py
+++ test/test_astgen.py
@@ -1,3 +1,4 @@
+from parser.common import StrRegion
 from test.common import CommonTestCase
 
 
@@ -6,5 +7,31 @@ class TestAstGen(CommonTestCase):
         self.assertAstMatchesSnapshot('s=s+9;')
 
     def test_nop_node(self):
-        self.assertAstMatchesSnapshot('if(2){;}else if (1==2){;a();;} else {};\n'
-                                      ';repeat(a){;};def fn(){;};')
+        self.assertAstMatchesSnapshot(
+            'if(2){;}else if (1==2){;a();;} else {};\n'
+            ';repeat(a){;};def fn(){;};')
+
+    def test_while_block(self):
+        self.assertAstMatchesSnapshot(
+            'while -1==1+-2 {};\n'
+            'while (Abc.d.lower()==msg[0].lower()){\n'
+            '  print("q" .. msg[0]);\n'
+            '}')
+
+    def test_aug_assign(self):
+        self.assertAstMatchesSnapshot(
+            'a += 6 + !0;\n'
+            'a.b[c .. "w"] **= (-0.5).q();'
+            'a.c ||= "\\t\\x1b[34;45m(\\"default\\")\\x1b[0m\\n";')
+
+    def test_error(self):
+        err = self.assertFailsGracefullyAST('3 + a;')
+        self.assertEqual(StrRegion(0, 5), err.region)
+        self.assertContains(err.msg.lower(), "expected statement")
+
+        err = self.assertFailsGracefullyAST('b*3 = 8;')
+        self.assertEqual(StrRegion(0, 3), err.region)
+        self.assertContains(err.msg.lower(), "cannot assign")
+
+    def test_getattr(self):
+        self.assertAstMatchesSnapshot('a.b[c].d = e.f[g].h[i];')
