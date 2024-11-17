0aecc03f11c70e0f64046d8cfb249fa8e1df5b4c
Marcell Perger <marcell.perger.1@gmail.com>
2024-11-17 21:48:15 +0000

    feat(ast): Add support for list literals in variable decls only

--- parser/astgen/astgen.py
+++ parser/astgen/astgen.py
@@ -134,14 +134,26 @@ class AstGen:
                 f"the root level.", smt.region)
 
     def _walk_var_decl(self, smt: DeclNode):
-        decls = [(self._walk_ident(d.ident),
-                  None if d.value is None else self._walk_expr(d.value))
+        decls = [(self._walk_ident(d.ident), self._walk_decl_value(d.value))
                  for d in smt.decl_list.decls]
         scope = (VarDeclScope.LET if isinstance(smt.decl_scope, DeclScope_Let)
                  else VarDeclScope.GLOBAL)
         tp = (VarType.LIST if isinstance(smt.decl_type, DeclType_List)
               else VarType.VARIABLE)
         return [AstDeclNode(smt.region, scope, tp, decls)]
+
+    def _walk_decl_value(self, value: AnyNode | None):
+        if value is None:
+            return None
+        if isinstance(value, ListNode):
+            # Special check (for now): list literals are disallowed everywhere
+            #  but here so _parse_expr raises an error when it finds a list
+            #  literal so we add this extra check as they *are* allowed here
+            return self._walk_list_literal_decl(value)
+        return self._walk_expr(value)
+
+    def _walk_list_literal_decl(self, ls: ListNode):
+        return AstListLiteral(ls.region, [self._walk_expr(i) for i in ls.items])
 
     def _walk_assign_left(self, lhs: AnyNode) -> AstNode:
         if isinstance(lhs, IdentNode):
@@ -194,6 +206,12 @@ class AstGen:
     def _walk_attr_name(self, attr_name: AttrNameNode) -> AstAttrName:
         return AstAttrName(
             attr_name.region, self.node_str(attr_name, intern=True))
+
+    @_register_autowalk_expr  # (we do a check in _parse_var_decl before calling _parse_expr)
+    def _walk_list_literal_expr(self, node: ListNode):
+        raise self.err("List literals are (for now) only supported in variable"
+                       " decls/initializers. (Note: nested list are not "
+                       "supported)", node)
 
     @_register_autowalk_expr
     def _walk_getattr(self, node: GetattrNode) -> AstAttribute:
