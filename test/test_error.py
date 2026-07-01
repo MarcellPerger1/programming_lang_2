e02664126943fe209f0dcdeea6a81ceb8d2c32bd
Marcell Perger <marcell.perger.1@gmail.com>
2026-07-01 11:43:58 +0100

    test: Fix mocks that tried to replace old method

--- test/test_error.py
+++ test/test_error.py
@@ -1,13 +1,28 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 from parser.common import StrRegion
 from parser.common.error import BaseLocatedError
 
 
+class _PatchRegionDisplay:
+    def __init__(self):
+        def _mocked_display(*args, **kwargs):
+            return self._mock(*args, **kwargs)
+        self._mock = MagicMock()
+        self._inner = patch.object(StrRegion, 'display', _mocked_display)
+
+    def __enter__(self):
+        self._inner.__enter__()
+        return self._mock
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return self._inner.__exit__(exc_type, exc_val, exc_tb)
+
+
 class TestLocatedError(unittest.TestCase):
     def test_adds_note_once(self):
-        with patch.object(BaseLocatedError, 'display_region') as mock_display:
+        with _PatchRegionDisplay() as mock_display:
             mock_display.return_value = 'mock_return_value'
             err = BaseLocatedError("a_message", StrRegion(0, 2), 'src_str')
             str(err)
@@ -16,15 +31,15 @@ class TestLocatedError(unittest.TestCase):
             self.assertEqual(len(err.get_notes()), 1)
 
     def test_uses_display_return_value(self):
-        with patch.object(BaseLocatedError, 'display_region') as mock_display:
+        with _PatchRegionDisplay() as mock_display:
             mock_display.return_value = 'mock_return_value'
             err = BaseLocatedError("a_message", StrRegion(0, 2), 'src_str')
             str(err)
-            mock_display.assert_called_once_with('src_str', StrRegion(0, 2))
+            mock_display.assert_called_once_with(StrRegion(0, 2), 'src_str')
             self.assertEqual(err.get_notes(), ['mock_return_value'])
 
     def test_only_calls_display_when_needed(self):
-        with patch.object(BaseLocatedError, 'display_region') as mock_display:
+        with _PatchRegionDisplay() as mock_display:
             mock_display.return_value = 'mock_return_value'
             err = BaseLocatedError("a_message", StrRegion(0, 2), 'src_str')
             mock_display.assert_not_called()
