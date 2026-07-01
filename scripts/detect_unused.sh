5ea93977ab4599b71d69aeae064dbc5cbd29c6c1
Marcell Perger <marcell.perger.1@gmail.com>
2026-07-01 19:07:25 +0100

    fix: Fix detect_unused.sh

--- scripts/detect_unused.sh
+++ scripts/detect_unused.sh
@@ -1,1 +1,1 @@
-vulture ./parser ./scripts/fuzz.py ./scripts/fuzz.py ./test --ignore-decorators="@_register_autowalk_expr*" --exclude="parser/astgen/ast_print.py"
+vulture ./parser ./scripts/fuzz.py ./scripts/benchmark.py ./test --ignore-decorators="@_register_autowalk_expr*" --exclude="parser/astgen/ast_print.py"
