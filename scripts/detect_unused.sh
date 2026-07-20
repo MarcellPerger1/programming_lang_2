568829fb8ed321fa096fa0c71a88a1993867bb03
Marcell Perger <marcell.perger.1@gmail.com>
2026-07-20 16:15:47 +0100

    ci(dead-code): Updated vulture script

--- scripts/detect_unused.sh
+++ scripts/detect_unused.sh
@@ -1,1 +1,2 @@
-vulture ./parser ./scripts/fuzz.py ./scripts/benchmark.py ./test --ignore-decorators="@_register_autowalk_expr*" --exclude="parser/astgen/ast_print.py"
+vulture ./parser ./scripts/fuzz.py ./scripts/benchmark.py ./test \
+ --ignore-decorators="@_register_autowalk_expr*,@_node_typechecker,@_TypecheckerInitVars.method,@register_corresponding_token"
